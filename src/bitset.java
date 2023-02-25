e5d57a304b60f36ae1a4d293df0eade20a1d1812
Павел <psobolev2004@gmail.com>
2023-02-25 22:37:50 +0300

    first version. Not full

--- src/bitset.java
+++ src/bitset.java
@@ -1,22 +1,46 @@
-public class bitset{
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+
+public class bitset {
 
     public static void main(String[] args) {
 
-        Person tom = new Person(); // создание объекта
-        tom.displayInfo();
+        BitSet b1 = new BitSet(4);
+        BitSet b2 = new BitSet(4);
+        BitSet b3 = new BitSet();
+        BitSet b4 = new BitSet();
 
-        // изменяем имя и возраст
-        tom.name = "Tom";
-        tom.age = 34;
-        tom.displayInfo();
-    }
-}
-class Person{
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-    String name;    // имя
-    String surname;    // имя
-    int age;        // возраст
-    void displayInfo(){
-        System.out.printf("Name: %s \tAge: %d\n", name, age);
+        b1.set(0, true);
+        b1.set(1, true);
+        b1.set(2, true);
+        b1.set(3, true);
+
+        b2.set(0, true);
+        b2.set(1, true);
+        b2.set(2, true);
+        b2.set(3, true);
+
+//        numbers.add(0);
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+
+        System.out.println("B1: " + b1);
+        System.out.println("B2: " + b2);
+
+        b1.and(b2);
+        System.out.println("B1 and B2: " + b1);
+
+        b1.or(b2);
+        System.out.println("B1 or B2: " + b1);
+
+        for (int i = 0; i < 4; i++) {
+
+        }
+        System.out.println("i in B1: " + b3);
     }
 }
